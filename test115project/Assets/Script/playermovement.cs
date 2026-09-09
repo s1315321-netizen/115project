03c44209ef7d232dd17ed38ebfdde26747e06a34
s1315321-netizen <s1315321@taivs.tp.edu.tw>
2026-09-10 00:08:15 +0800

    0

--- test115project/Assets/Script/playermovement.cs
+++ test115project/Assets/Script/playermovement.cs
@@ -2,37 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playermovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    
     public float speed = 10f;
+    public Animator anim;
 
-    // Start is called before the first frame update
+    private Vector3 initialScale;
+
     void Start()
     {
-
+        initialScale = transform.localScale;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //W/S 控制垂直移動
-        float v = 0f;
-        if (Input.GetKey(KeyCode.W)) v = 1f;
-        if (Input.GetKey(KeyCode.S)) v = -1f;
+        float h = Input.GetAxisRaw("Horizontal"); // A/D 或 左右箭頭
+        float v = Input.GetAxisRaw("Vertical");   // W/S 或 上下箭頭
 
-        //A/D 控制水平移動
-        float h = 0f;
-        if (Input.GetKey(KeyCode.D)) h = 1f;
-        if (Input.GetKey(KeyCode.A)) h = -1f;
+        Vector2 inputDir = new Vector2(h, v).normalized;
 
-        
-        Vector3 movement = new Vector3(h * speed * Time.deltaTime, v * speed * Time.deltaTime, 0f);
+        Vector3 movement = new Vector3(inputDir.x, inputDir.y, 0f) * speed * Time.deltaTime;
         transform.Translate(movement, Space.World);
-        
+
+        // 左右翻轉角色外觀
         if (h != 0f)
         {
-            transform.localScale=new Vector3(h, 1f, 1f);
+            float direction = Mathf.Sign(h);
+            transform.localScale = new Vector3(Mathf.Abs(initialScale.x) * direction, initialScale.y, initialScale.z);
+        }
+
+        // 更新 Animator 參數
+        if (anim != null)
+        {
+            // Speed > 0.01 代表有移動，0 代表站立
+            anim.SetFloat("Speed", inputDir.magnitude);
+            anim.SetFloat("Vertical", v);
+            anim.SetFloat("Horizontal", Mathf.Abs(h));
         }
     }
 }
